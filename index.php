908dbe6c301fd7802e37d8f9107f0b77ebaa80a7
ranrao <ranrao@yahoo.com>
2017-01-17 14:50:49 -0800

    0001: Initial files to enable login.

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+   ob_start();
+   session_start();
+
+  if ( !empty($_POST['username']) && !empty($_POST['password'])) {
+    $username = $_POST['username'];
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <title>RLogin</title>
+  <meta name="description" content="Sample Login Form">
+  <meta name="author" content="Ranjani">
+  <link rel="stylesheet" href="rlogin.css" type="text/css" media="screen" />
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="rlogin.js"></script>
+</head>
+
+<body>
+  <div id="header">
+    <div id='welcomeUser'>
+    <?php 
+      if(isset($username)) {
+        echo "Welcome, " . $username;
+      }
+    ?>
+    </div>
+    <div id="loginbtn">Login</div>
+  </div>
+  <div id="content">
+    <div id="rpopup" class="popup">
+      <form class="popup-content animate"  method = "post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+        <div class="container">
+          <div>
+            <label><img src="images/username.png" alt="Username" class="usernameIcon"></label>
+            <input type="text" placeholder="Username" name="username" required>
+          </div>
+          <div>
+            <label><img src="images/password.png" alt="Password" class="passwordIcon"></label>
+            <input type="password" placeholder="Password" name="password" required>
+          </div>
+          <button id="submitbtn" type="submit">LOGIN</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</body>
+
+</html>
---
+++ rlogin.js
@@ -0,0 +1,18 @@
+// JQuery begins ...
+$(document).ready(function() {
+
+  // Get the popup
+  var popup = document.getElementById('rpopup');
+
+  $('#loginbtn').on("click", function() {
+    popup.style.display = 'block';
+  });
+
+  // When the user clicks anywhere outside of the popup, close it
+  window.onclick = function(event) {
+    if (event.target == popup) {
+      popup.style.display = "none";
+    }
+  }
+
+});
